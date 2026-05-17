7853eb1470e53315a7c1035868927af822594bec
peashooter2 <3627668579@qq.com>
2026-05-17 17:18:25 +0800

    Fix: correct PARTICLE_VASE render layer from TOP to GROUND

--- src/Lawn/Challenge.cpp
+++ src/Lawn/Challenge.cpp
@@ -4163,7 +4163,7 @@ void Challenge::ScaryPotterOpenPot(GridItem* theScaryPot)
 		theScaryPot->mGridItemState == GRIDITEM_STATE_SCARY_POT_LEAF ? PARTICLE_VASE_SHATTER_LEAF :
 		theScaryPot->mGridItemState == GRIDITEM_STATE_SCARY_POT_ZOMBIE ? PARTICLE_VASE_SHATTER_ZOMBIE :
 		PARTICLE_VASE_SHATTER;
-	mApp->AddTodParticle(aXPos + 20, aYPos, RENDER_LAYER_TOP, anEffect);
+	mApp->AddTodParticle(aXPos + 20, aYPos, RENDER_LAYER_GROUND, anEffect);
 }
 
 void Challenge::ScaryPotterJackExplode(int thePosX, int thePosY)
